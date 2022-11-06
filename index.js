b077acbecbeb3819dd5cec9b9c36a1c83f48c343
Ganesh <59509677+Ganeshpy3@users.noreply.github.com>
2022-11-06 12:54:33 +0530

    s

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const data = require("./data.json")
 
-let port = process.env.port || 3000
+let port = process.env.PORT || 3000
 
 app.get("/",(req,res) =>{
     res.send("hello");
