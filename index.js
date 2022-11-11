44497b98dd610c67fe4bafc22c87c71070ad283a
Ganesh <59509677+Ganeshpy3@users.noreply.github.com>
2022-11-12 01:10:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {findRecord} = require("./db");
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  "mongodb+srv://root:6m3vFT2dYDEWAXBF@db.u7h1jzi.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.SERVER}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 
 client.connect().then( ()=>{
